6c258de294eda7b38a5119f11227e7b6f10652ea
Lynielo A. Mangco <111991848+lynielomangco@users.noreply.github.com>
2022-10-20 15:03:03 +0800

    Add files via upload

---
+++ act3.js
@@ -0,0 +1,8 @@
+function alphabet_order(str)
+  {
+    
+return str.split('').sort().join('');
+  }
+console.log(alphabet_order("lynielo"));
+console.log(alphabet_order("alfuerto"));
+console.log(alphabet_order("mangco"));
